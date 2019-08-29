b85fa8a974c3953f031294df6ab3771a7f27a133
Dong Qiu <dolphinqd@gmail.com>
2019-08-29 03:10:13 +0000

    update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 echo query --package_path $(pwd):$HOME/bin/base_workspace > ~/.bazelrc
 bazel query --noimplicit_deps 'deps(//source/server:listener_hooks_lib)' --output graph > graph.in
 dot -Tpng < graph.in > graph.png
+gp open graph.png
