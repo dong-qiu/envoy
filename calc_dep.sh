62e89c4a660fac807752972f231d371ebac40bb3
Dong Qiu <dolphinqd@gmail.com>
2019-08-29 12:22:40 +0000

    rename the shell script

--- calc_dep.sh
+++ calc_dep.sh
@@ -1,5 +1,7 @@
+echo "Calculating dependencies ..."
 echo query --package_path $(pwd):$HOME/bin/base_workspace > ~/.bazelrc
 bazel query --noimplicit_deps 'deps(//source/server:listener_hooks_lib)' --output graph > graph.in
-read -n1 -p "Press any key to continue..."
+echo "Visualizing dependencies ..."
 dot -Tpng < graph.in > graph.png
+echo "Opening dependency graph ..."
 gp open graph.png
