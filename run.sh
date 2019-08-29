9cfa0e22bc6327d50651dc397d230c21ff403532
Dong Qiu <dolphinqd@gmail.com>
2019-08-29 19:08:04 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 echo query --package_path $(pwd):$HOME/bin/base_workspace > ~/.bazelrc
 bazel query --noimplicit_deps 'deps(//source/server:listener_hooks_lib)' --output graph > graph.in
+read -n1 -p "Press any key to continue..."
 dot -Tpng < graph.in > graph.png
 gp open graph.png
